44fe6b76f4f0d97a4cee121d4fc35e6b5acff80a
willingdev0320 <amdmin0316@gmail.com>
2015-11-23 15:44:29 +0100

    Created first Button and unique Style -> Test declared in manifest

--- app/src/main/java/prak/travelerapp/MainActivity.java
+++ app/src/main/java/prak/travelerapp/MainActivity.java
@@ -3,14 +3,31 @@ package prak.travelerapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button newTrip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("dsd","dchih");
 
+        prepareViews();
+        prepareListeners();
+    }
+
+    private void prepareListeners() {
+        newTrip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("MainActivity","Clicked Started new Trip");
+            }
+        });
+    }
+
+    private void prepareViews() {
+        newTrip = (Button) findViewById(R.id.newTrip);
     }
 }
