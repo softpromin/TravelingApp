3836881c6da94317bc87ee42244ccbb83b050fd2
willingdev0320 <amdmin0316@gmail.com>
2015-12-07 13:46:29 +0100

    ImageView from URL as background

--- app/src/main/java/prak/travelerapp/FlickrAPI/LandingActivity.java
+++ app/src/main/java/prak/travelerapp/FlickrAPI/LandingActivity.java
@@ -1,15 +1,31 @@
 package prak.travelerapp.FlickrAPI;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 
 import prak.travelerapp.R;
 
 public class LandingActivity extends AppCompatActivity {
+
+    private Button sanFrancisco;    // Photo Test
+    private ImageView imageView;    // ImageView
+    public static final String URL = "http://c1.staticflickr.com/3/2882/13831351534_64c6d4e018_n.jpg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +42,79 @@ public class LandingActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        // Create an object for subclass of AsyncTask
+        GetXMLTask task = new GetXMLTask();
+        // Execute the task
+        task.execute(new String[] { URL });
+
+        sanFrancisco = (Button) findViewById(R.id.button);
+        sanFrancisco.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Log.d("Landing Activity", "JSON for San Francisco");
+
+            }
+        });
+    }
+
+    private class GetXMLTask extends AsyncTask<String, Void, Bitmap> {
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            Bitmap map = null;
+            for (String url : urls) {
+                map = downloadImage(url);
+            }
+            return map;
+        }
+
+        // Sets the Bitmap returned by doInBackground
+        @Override
+        protected void onPostExecute(Bitmap result) {
+            imageView.setImageBitmap(result);
+        }
+
+        // Creates Bitmap from InputStream and returns it
+        private Bitmap downloadImage(String url) {
+            Bitmap bitmap = null;
+            InputStream stream = null;
+            BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+            bmOptions.inSampleSize = 1;
+
+            try {
+                stream = getHttpConnection(url);
+                bitmap = BitmapFactory.
+                        decodeStream(stream, null, bmOptions);
+                stream.close();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+            return bitmap;
+        }
+
+        // Makes HttpURLConnection and returns InputStream
+        private InputStream getHttpConnection(String urlString)
+                throws IOException {
+            InputStream stream = null;
+            URL url = new URL(urlString);
+            URLConnection connection = url.openConnection();
+
+            try {
+                HttpURLConnection httpConnection = (HttpURLConnection) connection;
+                httpConnection.setRequestMethod("GET");
+                httpConnection.connect();
+
+                if (httpConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                    stream = httpConnection.getInputStream();
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            return stream;
+        }
     }
 
 }
