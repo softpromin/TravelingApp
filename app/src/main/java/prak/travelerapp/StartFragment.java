39c7177539e05c8419fa60ca1fef692106931522
willingdev0320 <amdmin0316@gmail.com>
2015-12-12 19:58:51 +0100

    Button onClick now changes Button Style --> give a button the drawable button as background

--- app/src/main/java/prak/travelerapp/StartFragment.java
+++ app/src/main/java/prak/travelerapp/StartFragment.java
@@ -1,16 +1,18 @@
 package prak.travelerapp;
 
 import android.app.Fragment;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.RadioGroup;
 
 public class StartFragment extends Fragment implements View.OnClickListener {
-
+    Button new_trip;
     ImageButton button_hamburger;
     RadioGroup radioGroup_gender;
     @Override
@@ -19,22 +21,23 @@ public class StartFragment extends Fragment implements View.OnClickListener {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_start, container, false);
 
+        new_trip = (Button) view.findViewById(R.id.newTrip);
+        new_trip.setOnClickListener(this);
+
         button_hamburger = (ImageButton) view.findViewById(R.id.button_hamburger);
         button_hamburger.setOnClickListener(this);
 
         radioGroup_gender = (RadioGroup) view.findViewById(R.id.radioGroup_gender);
-        radioGroup_gender.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
+        radioGroup_gender.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                switch(checkedId){
+                switch (checkedId) {
                     case R.id.radio_male:
                         Log.d("mw", "male");
                         break;
                     case R.id.radio_female:
                         Log.d("mw", "female");
                         break;
-
-
                 }
             }
         });
@@ -56,6 +59,8 @@ public class StartFragment extends Fragment implements View.OnClickListener {
             case R.id.button_hamburger:
                 ((MainActivity)getActivity()).openDrawer();
                 break;
+            case R.id.newTrip:
+                Log.d("Start Fragment","Clicked new Trip");
         }
 
     }
