99405f4c81956c928a50b1c68930de96a01ee4ad
willingdev0320 <amdmin0316@gmail.com>
2015-12-12 18:59:33 +0100

    Checkboxen

--- app/src/main/java/prak/travelerapp/StartFragment.java
+++ app/src/main/java/prak/travelerapp/StartFragment.java
@@ -2,14 +2,17 @@ package prak.travelerapp;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.RadioGroup;
 
 public class StartFragment extends Fragment implements View.OnClickListener {
 
     ImageButton button_hamburger;
+    RadioGroup radioGroup_gender;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -18,6 +21,23 @@ public class StartFragment extends Fragment implements View.OnClickListener {
 
         button_hamburger = (ImageButton) view.findViewById(R.id.button_hamburger);
         button_hamburger.setOnClickListener(this);
+
+        radioGroup_gender = (RadioGroup) view.findViewById(R.id.radioGroup_gender);
+        radioGroup_gender.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch(checkedId){
+                    case R.id.radio_male:
+                        Log.d("mw", "male");
+                        break;
+                    case R.id.radio_female:
+                        Log.d("mw", "female");
+                        break;
+
+
+                }
+            }
+        });
 
 
         return view;
